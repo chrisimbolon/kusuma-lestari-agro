9bda4d9f3f8b8bf28aff7a39f5c82ce61c5b39af
Christyan Simbolon <sambungdata@gmail.com>
2026-03-07 23:11:03 +0700

    feat: add Landingpage

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/common/Navbar"
+import Hero from "../sections/Hero"
+
+export default function LandingPage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
