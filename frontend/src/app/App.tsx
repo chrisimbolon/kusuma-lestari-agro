aa7e62828253917632cdba5c7b0999abf863d1b0
Christyan Simbolon <sambungdata@gmail.com>
2026-04-30 03:17:11 +0700

    feat-frontend: update App

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,6 @@
+import { router } from "@/routes/router";
 import { RouterProvider } from "react-router-dom";
-import { router } from "../routes/router";
-import Providers from "./providers";
+
 export default function App() {
-  return <Providers><RouterProvider router={router} /></Providers>;
+  return <RouterProvider router={router} />;
 }
